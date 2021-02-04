5cfb069b2c6fe9e54ce58cf8f593623c4f709ea0
Itay Barber <itaybarber@gmail.com>
2021-02-04 21:04:26 +0200

    mpas?

--- typescript/maps/src/index.ts
+++ typescript/maps/src/index.ts
@@ -7,7 +7,7 @@ const company = new Company();
 console.log(company);
 
 var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
+  zoom: 11,
   center: {
     lat: 0,
     lng: 0
