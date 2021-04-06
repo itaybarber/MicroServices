df5a78ae8df494372bad56ad1e80d53a62556e89
Itay Barber <itaybarber@gmail.com>
2021-04-06 18:22:09 +0300

    Update user.ts

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -39,7 +39,12 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre('save', async function(done) {
-    await Password. 
+     if (this.isModified('password')) {
+        const hashed = await Password.toHash(this.get('password')); 
+        this.set('password', hashed);
+    }
+    done();
+    
 });
 
 userSchema.statics.build = (attrs: UserAttrs) =>  {
