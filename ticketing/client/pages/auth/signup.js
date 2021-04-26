4b698b02beb5df968e5fd639f9816e3d98cdf57e
unknown <itaybarber@gmail.com>
2021-04-26 22:56:11 +0300

    making the possibilty to send data from browser

--- ticketing/client/pages/auth/signup.js
+++ ticketing/client/pages/auth/signup.js
@@ -1,14 +1,31 @@
+import {useState} from 'react'
+import axios from 'axios';
+
 export default () => {
+  const [email, setEmail] = useState(''); // Those are react hooks, to keep the state. Defaulted to an empty string
+  const [password, setPassword] = useState('');
+  
+  const onSubmit = async (event) => {
+    event.preventDefault(); // In order that the form won't sumbit itself to the browser
+
+    const response = await axios.post('/api/users/signup', {
+      email,
+      password
+    });
+
+    console.log(response.data); // axios is responsible to put the data from the request in response.data
+  }
+  
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className="form-group">
         <label>Email Address</label>
-        <input className="form-control" />
+        <input value={email} onChange={e =>setEmail(e.target.value)} className="form-control" />
       </div>
       <div className="form-group">
         <label>Password</label>
-        <input type="password" className="form-control" />
+        <input value={password} onChange={e => setPassword(e.target.value)} type="password" className="form-control" />
       </div>
       <button className="btn btn-primary">Sign Up</button>
     </form>
