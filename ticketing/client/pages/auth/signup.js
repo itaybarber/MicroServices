eee935af9d2653cc068a5b09505daf6a10b20510
unknown <itaybarber@gmail.com>
2021-04-26 23:18:17 +0300

    handling errors from UI

--- ticketing/client/pages/auth/signup.js
+++ ticketing/client/pages/auth/signup.js
@@ -4,16 +4,21 @@ import axios from 'axios';
 export default () => {
   const [email, setEmail] = useState(''); // Those are react hooks, to keep the state. Defaulted to an empty string
   const [password, setPassword] = useState('');
-  
+  const [errors, setErrors] = useState([]) // Defaulted to an empty array;
   const onSubmit = async (event) => {
     event.preventDefault(); // In order that the form won't sumbit itself to the browser
 
-    const response = await axios.post('/api/users/signup', {
-      email,
-      password
-    });
+    try {
+      const response = await axios.post('/api/users/signup', {
+        email,
+        password
+      });
 
-    console.log(response.data); // axios is responsible to put the data from the request in response.data
+      console.log(response.data); // axios is responsible to put the data from the request in response.data
+    } catch (err) {
+       // err.response.data  We're going to receive an error object that contains an array of errors, each error has a msg and possibly a field
+       setErrors(err.response.data.errors);  
+    }
   }
   
   return (
@@ -27,6 +32,13 @@ export default () => {
         <label>Password</label>
         <input value={password} onChange={e => setPassword(e.target.value)} type="password" className="form-control" />
       </div>
+      {
+        errors.length > 0 && <div className="alert alert-danger"> {/* errors.length is checked so we won't show the opsss the whole time   */}
+        <h4>Oppsssss...</h4>
+        <ul className="my-0">
+          {errors.map(err => <li key={err.message}>{err.message}</li>)}
+        </ul>
+      </div>}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
