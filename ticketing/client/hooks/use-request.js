95355e92a06f8016b2669ae816fc499eecdeab00
Itay Barber <itaybarber@gmail.com>
2021-04-27 19:32:24 +0300

    using a general hook for all POST/GET/DELETE/FETCH methods for all urls

---
+++ ticketing/client/hooks/use-request.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import {useState} from 'react';
+
+export default ({url, method, body}) => {
+  const [errors, setErrors] = useState(null);
+
+  const doRequest = async () => {
+    try {
+      setErrors(null);
+      const response = await axios[method](url, body);
+      if (onSuccess) {
+        onSuccess(response.data);
+      }
+
+      return response.data;  // axios is responsible to put the data from the request in response.data 
+    }
+    catch (err) {
+      setErrors(
+        <div className="alert alert-danger">
+        <h4>Oppsssss...</h4>
+        <ul className="my-0"> 
+      {/* err.response.data. We're going to receive an error object that contains an array of errors, each error has a msg and possibly a field */}
+          {err.response.data.errors.map(err => <li key={err.message}>{err.message}</li>)} 
+        </ul>
+      </div>
+      );
+    };
+
+    return {doRequest, errors}; // The defualt convention of react hooks is to return an array
+}};
--- ticketing/client/pages/auth/signup.js
+++ ticketing/client/pages/auth/signup.js
@@ -1,24 +1,27 @@
 import {useState} from 'react'
-import axios from 'axios';
+import Router from 'next/router'; // in order to navigate user around pages
+import useRequest from '../../hooks/use-request';
 
 export default () => {
   const [email, setEmail] = useState(''); // Those are react hooks, to keep the state. Defaulted to an empty string
   const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState([]) // Defaulted to an empty array;
+
+  const {doRequest, errors} = useRequest({
+    url: '/api/users/signup', 
+    method: 'post', 
+    body: {
+      email, 
+      password
+    },
+    onSuccess: () => Router.push('/')
+   }); // To navigate the user to main page
+
   const onSubmit = async (event) => {
     event.preventDefault(); // In order that the form won't sumbit itself to the browser
-
-    try {
-      const response = await axios.post('/api/users/signup', {
-        email,
-        password
-      });
-
-      console.log(response.data); // axios is responsible to put the data from the request in response.data
-    } catch (err) {
-       // err.response.data  We're going to receive an error object that contains an array of errors, each error has a msg and possibly a field
-       setErrors(err.response.data.errors);  
-    }
+    
+    await doRequest();
+    
+    
   }
   
   return (
@@ -32,13 +35,7 @@ export default () => {
         <label>Password</label>
         <input value={password} onChange={e => setPassword(e.target.value)} type="password" className="form-control" />
       </div>
-      {
-        errors.length > 0 && <div className="alert alert-danger"> {/* errors.length is checked so we won't show the opsss the whole time   */}
-        <h4>Oppsssss...</h4>
-        <ul className="my-0">
-          {errors.map(err => <li key={err.message}>{err.message}</li>)}
-        </ul>
-      </div>}
+      {errors}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
