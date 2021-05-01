a0642c46b2267623e40da5b0afb49589dac6abaa
unknown <itaybarber@gmail.com>
2021-05-01 19:45:01 +0300

    Updates

---
+++ ticketing/common/build/middelwares/current-user.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.currentUser = void 0;
+var jsonwebtoken_1 = __importDefault(require("jsonwebtoken")); // We want to decode our JWT
+// We want to figure out if a user is logged in and if he's
+// than extract out of that payload and set it on req.currentUser 
+var currentUser = function (req, res, next) {
+    var _a;
+    if (!((_a = req.session) === null || _a === void 0 ? void 0 : _a.jwt)) { // checking if there's no session or if there's no jwt prop
+        return next();
+    }
+    try {
+        var payload = jsonwebtoken_1.default.verify(req.session.jwt, process.env.JWT_KEY);
+        req.currentUser = payload;
+    }
+    catch (err) { }
+    next();
+};
+exports.currentUser = currentUser;
