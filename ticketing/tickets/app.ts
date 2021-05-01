c36b8f6e3f478a992805d0bb11436861d5f27f46
unknown <itaybarber@gmail.com>
2021-05-02 00:01:21 +0300

    ticketing serivce

---
+++ ticketing/tickets/app.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { NotFoundError, errorHandler} from '@itay_tix/common/build/index';
+
+const app = express();
+app.set('trust proxy', true);  // Traffic is being proxied to our app through Ingress Nginx. 
+                               // Express is gonna see that stuff is being proxide and by defualit
+                               // express is gonna is not going to trust that https connection 
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test' // Require to use an HTTPS connection in case we're not in test environment
+})
+);
+
+
+// When we throw the error, express is going to catch the error,
+// and sent it of to our middleware. 
+// That middleware is going to take this status code and it's going to call the 
+// Serialize Errors fucntion and going to generate a response and that
+// response is going to be sent to anyone trying to access this route
+app.all('*', async (req, res) => { 
+  throw new NotFoundError()
+});
+
+app.use(errorHandler);
+
+export {app};
