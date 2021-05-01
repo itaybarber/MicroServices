5c72f9fb47b4271c72a1b611ceb61056298c7fe7
unknown <itaybarber@gmail.com>
2021-05-01 17:15:01 +0300

    common

---
+++ ticketing/common/build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+var color = {
+    red: 10,
+    blue: 2,
+    green: 33
+};
+console.log(color);
---
+++ ticketing/common/src/index.ts
@@ -0,0 +1,13 @@
+interface Color {
+  red: number;
+  blue: number;
+  green: number;
+}
+
+const color: Color = {
+  red: 10,
+  blue: 2,
+  green: 322
+}
+
+console.log(color);
