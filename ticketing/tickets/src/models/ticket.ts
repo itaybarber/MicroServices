59a6dcbb585ef0013257df2f6aaed83715da709d
unknown <itaybarber@gmail.com>
2021-05-07 23:28:09 +0300

    Creating the mongoose model and scehma

--- ticketing/tickets/src/models/ticket.ts
+++ ticketing/tickets/src/models/ticket.ts
@@ -16,3 +16,32 @@ interface TicketDoc extends mongoose.Document {
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
 }
+
+// The 1st arg is an obj and we're going to list all the different props we want it to have
+const ticketSchenma = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true},
+  price: {
+    type: Number,
+    required: true},
+  userId: {
+    type: String,
+    required: true
+  }},
+   {
+    toJSON: {
+      transform(doc, ret) { // ret is the object that's going to be the json
+        ret.id = ret._id;
+        delete ret._id;
+      }
+    }
+  });
+
+  ticketSchenma.statics.build = (attrs: TicketAttrs) => {
+    return new Ticket(attrs);
+  };
+
+  const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchenma);
+
+  export {Ticket};
