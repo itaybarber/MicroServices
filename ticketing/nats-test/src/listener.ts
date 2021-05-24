c4d1e2e2fe988d37b6e293929156bb1e61c5f5da
unknown <itaybarber@gmail.com>
2021-05-24 23:07:47 +0300

    making it possible for several listeners will be availble

--- ticketing/nats-test/src/listener.ts
+++ ticketing/nats-test/src/listener.ts
@@ -1,17 +1,25 @@
 import nats, {Message} from 'node-nats-streaming';
-
+import { randomBytes} from 'crypto';
 
 console.clear();
 
-const stan = nats.connect('ticketing', '123', {
+                                       // The random bytes: for having a random client name
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222'
 });
 
 stan.on('connect', () => {
   console.log('listener connected to NATS');
 
+  const options = stan.subscriptionOptions()
+    .setManualAckMode(true);
 
-  const subscription = stan.subscribe('ticket:created');
+  const subscription = stan.subscribe(
+    // name of channel:
+    'ticket:created', 
+    // Queue groud name: (in case we have several instances of the listener and we don't want it to repeat the same)
+    'orders-service-queue-group',
+    options);
 
   subscription.on('message', (msg: Message) => {
     const data = msg.getData();
@@ -19,5 +27,7 @@ stan.on('connect', () => {
     if (typeof data === 'string') {
       console.log(`Received event #${msg.getSequence()}, with data: ${(data)}`);
     }
+
+    msg.ack();
   });
 });
