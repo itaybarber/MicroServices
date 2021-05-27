205a7046979225d5f9a4c6268b426c4fd1e8317e
Itay Barber <itaybarber@gmail.com>
2021-05-27 20:07:56 +0300

    having events as common

--- ticketing/nats-test/src/events/ticket-created-listener.ts
+++ ticketing/nats-test/src/events/ticket-created-listener.ts
@@ -4,12 +4,16 @@ import { Subjects } from './subjects';
 import { TicketCreatedEvent } from './ticket-created-event';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
-    subject: Subjects.TicketCreated = Subjects.TicketCreated;
-    queueGroupName = 'payments-service';
+  readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  queueGroupName = 'payments-service';
     
-    onMessage(data: any, msg: Message) : void {
-      console.log('Event data!', data);
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) : void {
+    console.log('Event data!', data);
+
+    console.log(data.id);
+    console.log(data.title);
+    console.log(data.price);
   
-      msg.ack();
-    }
+    msg.ack();
   }
+}
--- ticketing/nats-test/src/publisher.ts
+++ ticketing/nats-test/src/publisher.ts
@@ -1,5 +1,5 @@
 import nats from 'node-nats-streaming';
-
+import {TicketCreatedPublisher} from './events/ticket-created-publisher';
 
 console.clear();
 
@@ -9,18 +9,27 @@ const stan = nats.connect('ticketing','abc', {
 });
 
 // After successufly connection, stan is going to emit a "connected" event and we're gonna listen to it
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('publisher connected to nats');
-    
-  const dataToShare = JSON.stringify( {
-    id: '123',
-    title: 'Concert',
-    price: 20
-});
+   
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({
+      id: '123',
+      title: 'Concert',
+      price: 20
+    });
+  }
+  catch(err) {
+    console.log(err);
+  }
+//   const dataToShare = JSON.stringify( {
 
-  stan.publish('ticket:created', dataToShare, () => {
-    console.log('Event published, invoked after data publishing');
-  });
+// });
+
+//   stan.publish('ticket:created', dataToShare, () => {
+//     console.log('Event published, invoked after data publishing');
+//   });
 
 });
 
