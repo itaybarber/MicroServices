d4ddd67f24620f12aa81e92e2bbc50c133262312
unknown <itaybarber@gmail.com>
2021-06-13 21:51:35 +0300

    show.ts

--- ticketing/orders/src/routes/show.ts
+++ ticketing/orders/src/routes/show.ts
@@ -1,9 +1,22 @@
 import express, {Request, Response} from 'express';
+import {NotAuthorizedError, NotFoundError, requireAuth} from '@itay_tix/common/build/index';
+import {Order} from '../models/order';
+import {Ticket} from '../models/ticket';
 
 const router = express.Router();
 
-router.get('/api/orders/:orderId', async (req:Request, res: Response) => {
-  res.send({});
+router.get('/api/orders/:orderId', requireAuth,async (req:Request, res: Response) => {
+  const order = await Order.findById(req.params.orderId).populate('ticket'); //The populate is to find the order and simultaniously to find the ticket
+  
+  if (!order) {
+    throw new NotFoundError();
+  }
+
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+
+  res.send(order);
 });
 
 export {router as showOrderRouter};
