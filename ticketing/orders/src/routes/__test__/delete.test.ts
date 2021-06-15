33bb8e74960d1cf71827ac9adfdb87456faa0290
Itay Barber <itaybarber@gmail.com>
2021-06-15 17:11:24 +0300

    publish orders tesets

--- ticketing/orders/src/routes/__test__/delete.test.ts
+++ ticketing/orders/src/routes/__test__/delete.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import {app} from '../../app';
 import { Order, OrderStatus } from '../../models/order';
 import { Ticket} from '../../models/ticket';
+import {natsWrapper} from '../../nats-wrapper';
 
 it('Marks an order as cancelled', async () => {
   const ticket = Ticket.build({title: 'test', price: 10});
@@ -24,4 +25,22 @@ it('Marks an order as cancelled', async () => {
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
 
-it.todo('Emits an order cancelled event');
+it('Emits an order cancelled event', async () => {
+  const ticket = Ticket.build({title: 'test', price: 10});
+  await ticket.save();
+  
+  const user = global.signin();
+  const {body: order} = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ticketId: ticket.id})
+    .expect(201);
+
+  await request(app)
+  .delete(`/api/orders/${order.id}`)
+  .set('Cookie', user)
+  .send()
+  .expect(204);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
