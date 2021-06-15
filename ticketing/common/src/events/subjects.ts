4b3e3290012661f6c70aa6d686da602606208ad6
Itay Barber <itaybarber@gmail.com>
2021-06-15 10:13:00 +0300

    Updates

--- ticketing/common/src/events/subjects.ts
+++ ticketing/common/src/events/subjects.ts
@@ -1,7 +1,11 @@
 export enum Subjects {
     TicketCreated = 'ticket:created',
     TicketUpdated = 'ticket:updated',
+    
+    OrderCreated = 'order:created',
+    OrderCancelled = 'order:cancelled',
 }
+
 
 const printSubject = (subject: Subjects) => {
     
