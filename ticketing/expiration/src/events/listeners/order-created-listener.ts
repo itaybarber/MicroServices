1ab72e5ce9668ed5c67726863683eddd5742e59b
itaybarber <itaybarber@gmail.com>
2021-07-20 23:08:48 +0300

    listening to event Order created in indx Expiration srv

--- ticketing/expiration/src/events/listeners/order-created-listener.ts
+++ ticketing/expiration/src/events/listeners/order-created-listener.ts
@@ -1,12 +1,17 @@
 import { Listener, OrderCreatedEvent, Subjects } from "@itay_tix/common/build/index";
 import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
+import { expirationQueue } from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated ; 
   queueGroupName = queueGroupName;
 
-  onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-    
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    await expirationQueue.add({
+      orderId: data.id
+    });
+
+    msg.ack();
   }
 }
