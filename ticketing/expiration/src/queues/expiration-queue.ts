1895228ce07746e52fd5e032166df97c991b86b1
Itay Barber <itaybarber@gmail.com>
2021-07-20 21:56:50 +0300

    including payload

---
+++ ticketing/expiration/src/queues/expiration-queue.ts
@@ -0,0 +1,20 @@
+import  Queue  from "bull";
+
+interface Payload {
+  orderId: string
+}
+
+const expirationQueue = new Queue<Payload>(  // The payload is to make sure we're sending and receiving to our queue the right info
+  'order:expiration',
+  {
+    redis: {
+      host: process.env.REDIS_HOST
+  }
+});
+
+expirationQueue.process(async (job) => {  // The job param is similar to the msg obj in Nats. it is not the acutal data, rather
+  // wraps up our data and has some info about the job as well like id and the data we're sending with the job
+  console.log('lets publish expiration event completed for orderId', job.data.orderId)
+})
+
+export { expirationQueue };
