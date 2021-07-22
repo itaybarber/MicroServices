2f4ecb50e430bc16c0b9a3f4ee98c2ead5e21a77
Itay Barber <itaybarber@gmail.com>
2021-07-22 16:19:19 +0300

    creating listeners for orders in Payments serivce

---
+++ ticketing/payments/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+import { Listener, OrderCreatedEvent, OrderStatus, Subjects } from "@itay_tix/common/build/index";
+import { Message } from "node-nats-streaming";
+import { natsWrapper } from "../../../nats-wrapper";
+import { OrderCreatedListener } from "../order-created-listener";
+import { Order } from '../../../models/order';
+
+const setup = async () => {
+  const listener = new OrderCreatedListener(natsWrapper.client);
+  
+  const data : OrderCreatedEvent['data'] = {
+    id: mongoose.Types.ObjectId().toHexString(),
+    status: OrderStatus.Created,
+    version: 0,
+    userId: mongoose.Types.ObjectId().toHexString(),
+    expiresAt: 'askd';
+    ticket: {
+        id: mongoose.Types.ObjectId().toHexString(),
+        price: 213
+    },
+  };
+
+  //@ts-ignore
+  const msg: Message = {
+    ack: jest.fn()
+  };
+
+  return {listener, data, msg};
+}
+
+it('repilcates the order info', async () => {
+  const {listener, data, msg} = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const order = await Order.findById(data.id);
+
+  expect(order!.price).toEqual(data.ticket.price);
+
+})
