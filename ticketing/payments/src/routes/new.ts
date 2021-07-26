e8899db69374214c99a05e3363a929c15825d871
Itay Barber <itaybarber@gmail.com>
2021-07-26 14:05:00 +0300

    Creating a charge using Stripe

--- ticketing/payments/src/routes/new.ts
+++ ticketing/payments/src/routes/new.ts
@@ -9,6 +9,7 @@ import {
   NotAuthorizedError,
 } from '@itay_tix/common/build/index';
 import { Order } from '../models/order';
+import { stripe } from '../stripe';
 
 const router = express.Router();
 
@@ -33,6 +34,13 @@ router.post(
       throw new BadRequestError('Cannot pay for a cancelled order');
     }
 
+    await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100,
+      source: token
+    });
+
+    
     res.send({success:true});
   }
 );
